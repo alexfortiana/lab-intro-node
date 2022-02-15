d120af52fa669008d55ef27b7f2acbe633fc0e85
Alex Fortiana <alexfortiana@gmail.com>
2022-02-15 19:19:38 +0100

    we got it

--- index.js
+++ index.js
@@ -1,17 +1,73 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [], 
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    // if(item) {
+      this.items.push(item);
+      this.length += 1
 
-  get(pos) {}
+      this.items.sort(function (a, b) {
+        return a - b;
 
-  max() {}
+      });
 
-  min() {}
+    // }
 
-  sum() {}
+    return this.items
+  }
 
-  avg() {}
+  get(pos) {
+    if(pos <= this.length && pos >= 0){
+      return this.items[pos]
+
+    } else {
+      throw new Error('OutOfBounds');
+
+    }
+
+  }
+
+  max() {
+    if( this.items.length > 0) {
+      return Math.max(...this.items)
+    } else {
+      
+      throw new Error('EmptySortedList');
+    }
+    
+    
+  }
+
+  min() {
+    if( this.items.length > 0) {
+      return Math.min(...this.items)
+    } else {
+      
+      throw new Error('EmptySortedList');
+    }
+    
+  }
+
+  sum() {
+    return this.items.reduce((acc, elem) => {
+      return acc + elem
+
+
+    }, 0)
+  }
+
+  avg() {
+
+    
+    if(this.items.length > 0){
+    return this.sum(this.items) / this.length
+  } else {
+    throw new Error('EmptySortedList');
+  }
+
 }
-
+}
 module.exports = SortedList;
